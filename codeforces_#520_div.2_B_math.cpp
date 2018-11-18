7839e222bd2becef104ee867b29a135d1f70f505
DAMPER <44198800+dampers@users.noreply.github.com>
2018-11-19 04:17:52 +0900

    Update codeforces_#520_div.2_B_math.cpp

--- codeforces_#520_div.2_B_math.cpp
+++ codeforces_#520_div.2_B_math.cpp
@@ -1,5 +1,5 @@
 # include <bits/stdc++.h>
-/////////////////////////////WRONG///////////////////////////
+
 using namespace std;
 
 int ins [1000001];
@@ -29,33 +29,49 @@ int main()
 	n = 1;
 	for(i=2;i<1000001;i++)
 	{
-		if(ins[i] == 0) continue;
-		else
+		if(ins[i])
 		{
 			n = n*i;
 			k++;
 		}
 		if(cnt<ins[i]) cnt = ins[i];
 	}
+	int scnt = 0;
 	if(k>1)
 	{
 		if(cnt>16) tmp = 6;
-		if(cnt>8 && cnt<=16) tmp = 5;
+		if(cnt>8 && cnt <= 16) tmp = 5;
 		else if(cnt>4 && cnt<=8) tmp = 4;
 		else if(cnt>2 && cnt<=4) tmp = 3;
 		else if(cnt == 2) tmp = 2;
 		else if(cnt == 1) tmp = 0;
+		for(i=0;i<=n;i++)
+		{
+			if(ins[i] && ins[i] == cnt)
+			{
+				for(int j=1;j<20;j++)
+				{
+					if(cnt == pow(2,j))
+					{
+						scnt++;
+					}
+				}
+			}
+		}
+		if(scnt == k) tmp--;
 	}
 	else if(k==1)
 	{
-		if(cnt>16) tmp = 5;
-		if(cnt>8 && cnt<=16) tmp = 4;
-		else if(cnt>4 && cnt<=8) tmp = 3;
-		else if(cnt>2 && cnt<=4) tmp = 2;
+		if(cnt>16) tmp = 6;
+		if(cnt == 16) tmp = 4;
+		else if(cnt>8 && cnt<16) tmp = 5;
+		else if(cnt == 8) tmp = 3;
+		else if(cnt>4 && cnt<8) tmp = 4;
+		else if(cnt == 4) tmp = 2;
+		else if(cnt == 3) tmp = 3;
 		else if(cnt == 2) tmp = 1;
 		else if(cnt == 1) tmp = 0;
 	}
-	
 	printf("%d %d", n, tmp);
 	return 0;
 }
