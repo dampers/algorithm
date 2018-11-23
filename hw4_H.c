f8a656adbf54462066ad836772ddbfb297e6619d
DAMPER <44198800+dampers@users.noreply.github.com>
2018-11-23 18:59:31 +0900

    Update hw4_H.c

--- hw4_H.c
+++ hw4_H.c
@@ -1,22 +1,22 @@
 # include <stdio.h>
-# include <string.h>
-
+  
 char alp[101];
-
+  
 int main()
 {
-	int n, m, i, j, k = 0, cnt, a;
-	scanf("%d %d", &n, &m);
-	scanf(" %s", alp);
-	cnt = n/m;
-	for(i=0;i<m;i++)
-	{
-		k += cnt;
-		for(a=k-1;a>=k-cnt;a--)
-		{
-			printf("%c", alp[a]);
-		}
-	}
-	printf("\n\n");
-	return 0;
+    int n, m, i, j, k = 0, cnt, a;
+    scanf("%d %d", &n, &m);
+    getchar();
+    gets(alp);
+    cnt = n/m;
+    for(i=0;i<m;i++)
+    {
+        k += cnt;
+        for(a=k-1;a>=k-cnt;a--)
+        {
+            printf("%c", alp[a]);
+        }
+    }
+    printf("\n\n");
+    return 0;
 }
