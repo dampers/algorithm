746f4b659bbcd51a6a7b81b5aabb6aea2de88d29
DAMPER <44198800+dampers@users.noreply.github.com>
2018-11-23 19:00:06 +0900

    Update hw4_G-1.c

--- hw4_G-1.c
+++ hw4_G-1.c
@@ -1,33 +1,34 @@
 # include <stdio.h>
 # include <string.h>
-# include <stdlib.h>
-
+  
 struct run
 {
-	char name[21];
-	int rec;
+    char name[21];
+    int rec;
 };
-
+  
 struct run *search[31];
 char sname[21];
-
+  
 int main()
 {
-	int n, i;
-	scanf("%d", &n);
-	for(i=0;i<n;i++)
-	{
-		search[i] = (struct run *) malloc(sizeof(struct run));
-		scanf("%s", search[i]->name);
-		scanf(" %d", &search[i]->rec);
-	}
-	scanf(" %s", sname);
-	for(i=0;i<n;i++)
-	{
-		if(!strcmp(sname, search[i]->name))
-		{
-			printf("%d\n\n", search[i]->rec);
-		}
-	}
-	return 0;
+    int n, i;
+    scanf(" %d", &n);
+    for(i=0;i<n;i++)
+    {
+        search[i] = (struct run *) malloc(sizeof(struct run));
+        getchar();
+        gets(search[i]->name);
+        scanf(" %d", &search[i]->rec);
+    }
+    getchar();
+    gets(sname);
+    for(i=0;i<n;i++)
+    {
+        if(!strcmp(sname, search[i]->name))
+        {
+            printf("%d\n\n", search[i]->rec);
+        }
+    }
+    return 0;
 }
