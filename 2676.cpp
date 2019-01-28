6c2ec0e010e97876a84c4e0d4b36bc3fdb517d0e
DAMPER <44198800+dampers@users.noreply.github.com>
2019-01-29 03:25:01 +0900

    Add files via upload

--- 2676.cpp
+++ 2676.cpp
@@ -2,29 +2,39 @@
 
 using namespace std;
 typedef long long lld;
-int r[50000][50000];
+
 int main()
 {
 	int n, m, t;
-	r[0][0] = 1;
-	for(int j=1;j<=50000;j++)
-		{
-			for(int k=0;k<=j;k++)
-			{
-				if(k==0) r[j][k] = 1;
-				else if(k==j)
-				{
-					r[j][k] = 1;
-					break;
-				}
-				else r[j][k] = (r[j-1][k-1]*r[j-1][k]+1)/r[j-2][k-1];
-			}
-		}
 	scanf(" %d", &t);
 	for(int i=0;i<t;i++)
 	{
 		scanf(" %d %d", &n, &m);
-		printf("%d\n", r[n][m]);
+		lld h, ans = n;
+		if(n>=2) h = n-3;
+		if(m == 0 || m == n)
+			printf("1\n");
+		else if(m == 1 || m == n-1)
+			printf("%d\n", n);
+		else
+		{
+			for(int j=2;j<=n/2;j++,h-=2)
+			{
+				ans += h;
+				if(j == m)
+				{
+					break;
+				}
+				if(m>n/2 && n-j==m)
+				{
+					break;
+				}
+			}
+			if(n%2==1 && m == n/2+1)
+				printf("%lld\n", ans);
+			else
+				printf("%lld\n", ans);
+		}
 	}
 	return 0;
 }
