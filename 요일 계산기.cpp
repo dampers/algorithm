1ec639e5b983b5fbaee0d27c2be698ea06bc0ac1
DAMPER <44198800+dampers@users.noreply.github.com>
2019-06-29 04:10:14 +0900

    Update 요일 계산기.cpp

--- 요일 계산기.cpp
+++ 요일 계산기.cpp
@@ -11,6 +11,15 @@ int main()
 	int y = 2007, m, d;
 	cout<<"요일을 알고싶은 날짜를 입력하세요 (ex) 2019 06 29) :";
 	cin>>y>>m>>d;
+	if((y%4==0 && y%100!=0) || y%400==0) f = 1;
+	else f = 0;
+	for(int i=1;i<m;i++)
+	{
+		if(i==2) d += 28+f;
+		else if(i<=7 && i%2==1) d += 31;
+		else if(i>7 && i%2==0) d += 31;
+		else d += 30;
+	}
 	if(y>=2007)
 	{
 		for(int i=0;i<y-2007;i++)
@@ -25,20 +34,11 @@ int main()
 	{
 		for(int i=0;i<2007-y;i++)
 		{
-			if(i%4==0 && i%100!=0 && i%400==0) f = 1;
+			int asd = y+i;
+			if((asd%4==0 && asd%100!=0) || asd%400==0) f = 1;
 			else f = 0;
-			d -= f+1;
-			if(d<1) d += 7;
+			d -= (f+1);
 		}
-	}
-	if(y%4==0 && y%100!=0 && y%400==0) f = 1;
-	else f = 0;
-	for(int i=1;i<m;i++)
-	{
-		if(i==2) d += 28+f;
-		else if(i<=7 && i%2==1) d += 31;
-		else if(i>7 && i%2==0) d += 31;
-		else d += 30;
 	}
 	switch(d%7)
 	{
