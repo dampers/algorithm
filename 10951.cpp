b5ee0a3695ecaa5cf83977681c8480ca90e1258b
DAMPER <44198800+dampers@users.noreply.github.com>
2020-04-20 17:11:50 +0900

    Add files via upload

--- 10951.cpp
+++ 10951.cpp
@@ -1,13 +1,16 @@
-# include <bits/stdc++.h>
-
+#include <bits/stdc++.h>
+#define endl '\n'
 using namespace std;
 
 int main()
 {
+	ios_base :: sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 	int a, b;
-	while(scanf("%d %d", &a, &b)==2)
+	while(cin>>a>>b)
 	{
-		printf("%d\n", a+b);
+		cout << a+b << endl;
 	}
 	return 0;
 }
