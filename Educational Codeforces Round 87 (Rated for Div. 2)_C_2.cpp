ef301dfdaf5ca76e91f93ad3ebdcc710902a5d8a
DAMPER <44198800+dampers@users.noreply.github.com>
2020-05-17 20:54:31 +0900

    Add files via upload

---
+++ Educational Codeforces Round 87 (Rated for Div. 2)_C_2.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define swap(a,b) (a)^=(b)^=(a)^=(b)
+#define endl '\n'
+using namespace std;
+typedef long long lld;
+
+#define PI 3.1415926535
+
+int main()
+{
+	ios_base::sync_with_stdio(NULL);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int tc;
+	scanf("%d", &tc);
+	while(tc--)
+	{
+		double n, k;
+		scanf("%lf", &n);
+		n *= 2*(1.0);
+		double r = 1/(2*sin(PI/n));
+		printf("%.7lf\n", 2*r*sin(((n-1)*PI)/(2*n)));
+	}
+	return 0;
+}
