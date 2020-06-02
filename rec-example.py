55c48a3cc600523c5bd83a60f445f9f8159fa67f
DAMPER <44198800+dampers@users.noreply.github.com>
2020-06-02 23:01:40 +0900

    Add files via upload

---
+++ rec-example.py
@@ -0,0 +1,21 @@
+for i in range(1, 4):
+    for j in range(1, 4):
+        for k in range(1, 4):
+            print(i, j, k)
+
+#3중 for문
+
+111~333
+
+for i in range(1, 5):
+    for j in range(1, 5):
+        for k in range(1, 5):
+            for l in range(1, 5):
+                print(i, j, k, l)
+
+1111~4444
+# 4중 for문
+
+n = int(input())
+
+# n중 for문?
