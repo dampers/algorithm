d678c19a1f75f06ff525be22e3995f7dccbe7f28
DAMPER <44198800+dampers@users.noreply.github.com>
2020-08-08 05:58:22 +0900

    Add files via upload

---
+++ Codeforces Round #662 (Div. 2)_A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define swap(a,b) (a)^=(b)^=(a)^=(b)
+#define endl '\n'
+using namespace std;
+typedef long long lld;
+
+int main()
+{
+	ios_base::sync_with_stdio(NULL);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int tc;
+	cin>>tc;
+	while(tc--)
+	{
+		lld n;
+		cin>>n;
+		cout<<n/2+1<<endl;
+	}
+	return 0;
+}
