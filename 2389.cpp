3ba5f38770eb69b43598c53e9a3e2df2fff010d1
DAMPER <44198800+dampers@users.noreply.github.com>
2020-09-11 05:24:06 +0900

    Add files via upload

--- 2389.cpp
+++ 2389.cpp
@@ -1,40 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define swap(a,b) (a)^=(b)^=(a)^=(b)
+typedef long long lld;
 
-struct coord
+double dst(double x, double y)
 {
-	double x, y;
-};
-
-coord cor[101];
+	return x*x+y*y;
+}
 
 int main()
 {
+	ios_base::sync_with_stdio(NULL);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
 	int n;
-	scanf(" %d", &n);
-	double mx = 0, d, xx, yy, tmp, mxx1, myy1, mxx2, myy2;
+	cin>>n;
+	vector<pair<double, double>> v;
+	double cx = 0.0, cy = 0.0;
 	for(int i=0;i<n;i++)
-		scanf(" %lf %lf", &cor[i].x, &cor[i].y);
-	for(int i=0;i<n-1;i++)
 	{
-		for(int j=i+1;j<n;j++)
+		double x, y;
+		cin>>x>>y;
+		v.push_back({x, y});
+		cx += x;
+		cy += y;
+	}
+
+	cx /= n;
+	cy /= n;
+
+	double r = 0.1, mx = 0.0;
+	for(int i=0;i<10000;i++)
+	{
+		int ind = 0;
+		mx = 0.0;
+		for(int j=0;j<n;j++)
 		{
-			xx = pow(abs(cor[i].x-cor[j].x), 2);
-			yy = pow(abs(cor[i].y-cor[j].y), 2);
-			tmp = pow((xx+yy), 0.5);
-			if(mx<tmp)
+			double d = dst(cx-v[j].first, cy-v[j].second);
+			if(mx<d)
 			{
-				mxx1 = cor[i].x;
-				myy1 = cor[i].y;
-				mxx2 = cor[j].x;
-				myy2 = cor[j].y;
-				mx = tmp;
+				ind = j;
+				mx = d;
 			}
 		}
+		cx += (v[ind].first-cx)*r;
+		cy += (v[ind].second-cy)*r;
+		r *= 0.999;
 	}
-	xx = mxx1-(mxx1-mxx2)/2;
-	yy = myy1-(myy1-myy2)/2;
-	mx /= 2;
-	printf("%.3lf %.3lf %.3lf", xx, yy, mx);
+	cout<<cx<<' '<<cy<<' '<<sqrt(mx);
 	return 0;
 }
