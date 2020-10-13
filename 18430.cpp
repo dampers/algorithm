51bf815e622524bf9e836b145fba6cf6a121fdad
DAMPER <44198800+dampers@users.noreply.github.com>
2020-10-14 00:16:13 +0900

    Update 18430.cpp

--- 18430.cpp
+++ 18430.cpp
@@ -4,31 +4,36 @@
 using namespace std;
 typedef long long lld;
 
-int n, m, mx;
+int n, m;
 int bm[6][6];
 bool check[6][6];
 
-int dfs(int y, int x, int c)
-{
+int dy[4][2] = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+int dx[4][2] = {{-1, 0}, {-1, 0}, {0, 1}, {0, 1}};
 
+int backtrack(int y, int x, int point)
+{
+	int ret = point;
 	for(int i=y;i<n;i++)
 	{
-		for(int j=x;j<m-1;j++)
+		for(int j=x;j<m;j++)
 		{
-			int sk = 0;
-			if(check[i][j]) continue;
-			check[i][j] = true;
-			if(i+1<n && j+1<m && !check[i][j+1] && !check[i+1][j])
+			if(check[i][j]==true) continue;
+			for(int d=0;d<4;d++)
 			{
-				check[i][j+1] = check[i+1][j] = true;
-				sk = bm[i][j]*2+bm[i][j+1]+bm[i+1][j];
-				mx = max(c+sk, c+sk+dfs())
-				check[i][j+1] = check[i+1][j] = false;
-
+				int ny1 = i+dy[d][0], ny2 = i+dy[d][1];
+				int nx1 = j+dx[d][0], nx2 = j+dx[d][1];
+				if(ny1>=n || ny2>=n || ny1<0 || ny2<0) continue;
+				else if(nx1>=m || nx2>=m || nx1<0 || nx2<0) continue;
+				else if(check[ny1][nx1] || check[ny2][nx2]) continue;
+				check[i][j] = check[ny1][nx1] = check[ny2][nx2] = true;
+				ret = max(ret, point+backtrack(i, j, bm[i][j]*2+bm[ny1][nx1]+bm[ny2][nx2]));
+				check[i][j] = check[ny1][nx1] = check[ny2][nx2] = false;
 			}
-
 		}
+		x = 0;
 	}
+	return ret;
 }
 
 int main()
@@ -40,6 +45,6 @@ int main()
 	for(int i=0;i<n;i++)
 		for(int j=0;j<m;j++)
 			cin>>bm[i][j];
-
+	cout<<backtrack(0, 0, 0);
 	return 0;
 }
