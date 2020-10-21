78008acbb1a819bf95144c956b06dcc11c813834
DAMPER <44198800+dampers@users.noreply.github.com>
2020-10-21 22:03:05 +0900

    Update karatsuba.cpp

--- karatsuba.cpp
+++ karatsuba.cpp
@@ -6,7 +6,7 @@ typedef long long lld;
 
 void addto(vector<int>& a, vector<int>& b, int k)
 {
-    a.resize(max(a.size()+1, b.size()+k));
+    a.resize(max(a.size()+1, b.size()+k), 0);
     int carry = 0;
     for(int i=0;i<b.size();i++)
     {
