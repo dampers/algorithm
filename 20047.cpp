ae8115a8be0dc8cb9a208ff74fabeadc6e8253ae
DAMPER <44198800+dampers@users.noreply.github.com>
2020-10-27 02:17:00 +0900

    Update 20047.cpp

--- 20047.cpp
+++ 20047.cpp
@@ -6,17 +6,17 @@ typedef long long lld;
 
 int n, l, r;
 
-bool brute_force(string& a, string& b, int pos, int n, int check, int aindex)
+bool dp(string& a, string& b, int pos, int n, int check, int aindex)
 {
 	if(pos==n) return true;
 	bool ret = false;
 	if(aindex==l || aindex==r) aindex++;
 	if(aindex<n && b[pos]==a[aindex])
-		ret |= brute_force(a, b, pos+1, n, check, aindex+1);
+		ret |= dp(a, b, pos+1, n, check, aindex+1);
 	if(check==0 && b[pos]==a[l])
-		ret |= brute_force(a, b, pos+1, n, 1, aindex);
+		ret |= dp(a, b, pos+1, n, 1, aindex);
 	if(check==1 && b[pos]==a[r])
-		ret |= brute_force(a, b, pos+1, n, 2, aindex);
+		ret |= dp(a, b, pos+1, n, 2, aindex);
 	return ret;
 }
 
@@ -29,7 +29,7 @@ int main()
 	string a, b;
 	cin>>a>>b;
 	cin>>l>>r;
-	if(brute_force(a, b, 0, n, 0, 0)) cout<<"YES";
+	if(dp(a, b, 0, n, 0, 0)) cout<<"YES";
 	else cout<<"NO";
 	return 0;
 }
