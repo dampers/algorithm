1b7e80d6d14250f10be21f1ce6c64caae5f84673
DAMPER <44198800+dampers@users.noreply.github.com>
2021-01-03 00:22:02 +0900

    Add files via upload

--- karatsuba.cpp
+++ karatsuba.cpp
@@ -6,7 +6,7 @@ typedef long long lld;
 
 void addto(vector<int>& a, vector<int>& b, int k)
 {
-    a.resize(max(a.size()+1, b.size()+k), 0);
+    a.resize(max(a.size()+2, b.size()+k+1), 0);
     int carry = 0;
     for(int i=0;i<b.size();i++)
     {
@@ -14,8 +14,6 @@ void addto(vector<int>& a, vector<int>& b, int k)
         carry = a[i+k]/10;
         a[i+k] %= 10;
     }
-    if(carry!=0 && a.back()!=0) a.push_back(1);
-    else if(carry!=0 && a.back()==0) a[a.size()-1] = 1;
     while(a.size()>=1 && a.back()==0) a.pop_back();
 }
 
@@ -35,20 +33,36 @@ void subfrom(vector<int>& a, vector<int>& b)
     }
 }
 
+vector<int> multiple(vector<int>& a, vector<int>& b)
+{
+    // a.size() >= b.size()
+    vector<int> res(a.size()+b.size()+2, 0);
+    for(int i=0;i<b.size();i++)
+    {
+        for(int j=0;j<a.size();j++)
+        {
+            res[i+j] += a[j]*b[i];
+        }
+    }
+    int carry = 0;
+    for(int i=0;i<res.size();i++)
+    {
+        res[i] += carry;
+        carry = res[i]/10;
+        res[i] %= 10;
+    }
+    while(res.back()==0) res.pop_back();
+    return res;
+}
+
 
 vector<int> karatsuba(vector<int>& a, vector<int>& b)
 {
     int alen = a.size(), blen = b.size();
     if(alen<blen) return karatsuba(b, a);
-    if(alen<=0 || blen<=0) return vector<int>();
-    else if(alen==1 && blen==1)
-    {
-        int tmp = a[0]*b[0];
-        vector<int> tmpv;
-        tmpv.push_back(tmp%10);
-        if(tmp>9) tmpv.push_back(tmp/10);
-        return tmpv;
-    }
+    if(alen==0 || blen==0) return vector<int>();
+    else if(alen<=230)
+        return multiple(a, b);
 
     int half = alen/2;
     vector<int> a0(a.begin(), a.begin()+half);
@@ -80,15 +94,15 @@ int main()
     time_t start = clock();
     string ai, bi;
     cin>>ai>>bi;
-    vector<int> a(ai.length()), b(bi.length());
+    vector<int> a(ai.length(), 0), b(bi.length(), 0);
     for(int i=0;i<ai.length();i++)
-        a[i] = ai[i]-'0';
+        a[i] = ai[ai.length()-i-1]-'0';
     for(int i=0;i<bi.length();i++)
-        b[i] = bi[i]-'0';
-    reverse(a.begin(), a.end());
-    reverse(b.begin(), b.end());
+        b[i] = bi[bi.length()-i-1]-'0';
     vector<int> ans = karatsuba(a, b);
     for(int i=ans.size()-1;i>=0;i--)
         cout<<ans[i];
+    cout<<endl;
+    cout<<((double)clock()-start)/1000;
     return 0;
 }
