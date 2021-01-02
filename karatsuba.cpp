5bf9be51780a246dde092f3bc55fc9be24ff97de
DAMPER <44198800+dampers@users.noreply.github.com>
2021-01-03 01:33:08 +0900

    Add files via upload

--- karatsuba.cpp
+++ karatsuba.cpp
@@ -6,13 +6,14 @@ typedef long long lld;
 
 void addto(vector<int>& a, vector<int>& b, int k)
 {
-    a.resize(max(a.size()+2, b.size()+k+1), 0);
+    a.resize(max(a.size()+1, b.size()+k+1), 0);
     int carry = 0;
-    for(int i=0;i<b.size();i++)
+    for(int i=0;i<b.size();i++) a[i+k] += b[i];
+    for(int i=0;i<a.size();i++)
     {
-        a[i+k] += (b[i]+carry);
-        carry = a[i+k]/10;
-        a[i+k] %= 10;
+        a[i] += carry;
+        carry = a[i]/10;
+        a[i] %= 10;   
     }
     while(a.size()>=1 && a.back()==0) a.pop_back();
 }
@@ -23,11 +24,7 @@ void subfrom(vector<int>& a, vector<int>& b)
     for(int i=0;i<a.size();i++)
     {
         if(i<b.size()) a[i] -= b[i]+bor;
-        else
-        {
-            a[i] -= bor;
-            if(!bor) break;
-        }
+        else a[i] -= bor;
         if(a[i]<0)
         {
             a[i] += 10;
@@ -69,10 +66,13 @@ vector<int> karatsuba(vector<int>& a, vector<int>& b)
         return multiple(a, b);
 
     int half = alen/2;
-    vector<int> a0(a.begin(), a.begin()+half), a1(a.begin()+half, a.end());
-    vector<int> b0(b.begin(), b.begin()+min((int)b.size(), half)), b1(b.begin()+min((int)b.size(), half), b.end());
+    vector<int> a0(a.begin(), a.begin()+half);
+    vector<int> a1(a.begin()+half, a.end());
+    vector<int> b0(b.begin(), b.begin()+min((int)b.size(), half));
+    vector<int> b1(b.begin()+min((int)b.size(), half), b.end());
 
-    vector<int> z2 = karatsuba(a1, b1), z0 = karatsuba(a0, b0);
+    vector<int> z2 = karatsuba(a1, b1);
+    vector<int> z0 = karatsuba(a0, b0);
     addto(a0, a1, 0);
     addto(b0, b1, 0);
     vector<int> z1 = karatsuba(a0, b0);
