5f5a568014805f24263cb2e2d84768aadfa03bc1
DAMPER <44198800+dampers@users.noreply.github.com>
2021-01-05 01:45:17 +0900

    Add files via upload

--- 1541.cpp
+++ 1541.cpp
@@ -1,91 +1,55 @@
 #include <bits/stdc++.h>
+using namespace std;
 #define swap(a,b) (a)^=(b)^=(a)^=(b)
 #define endl '\n'
-using namespace std;
 typedef long long lld;
-
-char a[55];
 
 int main()
 {
 	ios_base::sync_with_stdio(NULL);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	scanf(" %s", a);
-	int len = strlen(a);
-	vector<int> num;
-	vector<char> oper;
-	bool kflag = false;
-	for(int i=0;i<len;i++)
+	
+	string s;
+	cin>>s;
+	
+	vector<int> v;
+	queue<char> op;
+	v.push_back(0);
+	for(int i=0;i<s.length();i++)
 	{
-		if(kflag) break;
-		if(a[i]=='+') oper.push_back('+');
-		else if(a[i]=='-') oper.push_back('-');
-		else
+		if(s[i]=='-' || s[i]=='+')
 		{
-			int tmp = 0;
-			for(int j=i;j<i+5&&j<len;j++)
-			{
-				if(a[j]>='0'&&a[j]<='9')
-				{
-					tmp += a[j]-'0';
-					tmp *= 10;
-					if(j==len-1)
-					{
-						tmp /= 10;
-						kflag = true;
-						break;
-					}
-				}
-				else
-				{
-					tmp /= 10;
-					i = j-1;
-					break;
-				}
-			}
-			num.push_back(tmp);
+			op.push(s[i]);
+			v.push_back(0);
+		}
+		else if(s[i]>='0' && s[i]<='9')
+		{
+			*(v.end()-1) *= 10;
+			*(v.end()-1) += s[i]-'0';
 		}
 	}
-	int sum = num[0], numsize = num.size(), opsize = oper.size(), op1 = 1, op2 = 0, tmp = 0;
-	bool flag = false;
-	/*for(int i=0;i<numsize;i++)
-		printf("%d ", num[i]);*/
-	while(op1<numsize && op2<opsize)
+
+
+	lld ans = (lld)v[0], sub = 0;
+	for(int i=1;i<v.size();i++)
 	{
-		//printf("sum = %d\n", sum);
-		//printf("tmp = %d\n", tmp);
-		if(oper[op2]=='+')
+		if(op.front()=='-')
 		{
-			if(flag) tmp += num[op1++];
-			else sum += num[op1++];
+			ans -= sub;
+			sub = 0;
+			sub += v[i];
 		}
-		else if(oper[op2]=='-')
+		if(op.front()=='+')
 		{
-			if(flag)
-			{
-				if(op1+1<numsize && num[op1]<num[op1+1] && op2+1<opsize && oper[op2+1]=='+')
-				{
-					tmp -= num[op1++];
-					continue;
-				}
-				flag = false;
-				sum -= tmp;
-				tmp = 0;
-			}
-			else
-			{
-				if(op2+1<opsize && oper[op2+1]=='-')
-				{
-					sum -= num[op1++];
-					continue;
-				}
-				flag = true;
-				tmp = num[op1++];
-			}
+			if(sub) sub += v[i];
+			else ans += v[i];
 		}
-		printf("%d\n", sum);
+		op.pop();
 	}
-	printf("%d", sum);
+	if(sub) ans -= sub;
+	cout<<ans;
+
+
 	return 0;
 }
