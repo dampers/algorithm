21491b1c8c5bad49d3285b0588a400e8b706f1ab
DAMPER <44198800+dampers@users.noreply.github.com>
2021-02-02 01:28:17 +0900

    Add files via upload

--- algospot_NERD2.cpp
+++ algospot_NERD2.cpp
@@ -10,37 +10,48 @@ int main()
 	cin.tie(NULL);
 	cout.tie(NULL);
 	
-
 	int tc;
 	cin>>tc;
 	while(tc--)
 	{
-		int np, nq, n;
+		int n;
 		cin>>n;
 		map<int, int> mp;
 		int ans = 0;
 		for(int i=0;i<n;i++)
 		{
-			cin>>np>>nq;
-			
-			auto it = mp.lower_bound(np);
-			// can insert
-			if(it == mp.begin())
+			int p, q;
+			cin>>p>>q;
+			auto it = mp.lower_bound(p);
+			if(it == mp.end() || q>it->second)
 			{
-				if(mp.begin() == mp.end() || mp.begin()->second < np) mp[np] = nq;
-			}
-			else mp[np] = nq;
-
-			// erase
-			it = mp.lower_bound(np);
-			for(auto k = mp.begin(); k != it;)
-			{
-				if(k->second > nq) k++;
-				else k = mp.erase(k);
+				if(it != mp.begin())
+				{
+					--it;
+					while(1)
+					{
+						if(it->second > q) break;
+						if(it == mp.begin())
+						{
+							mp.erase(it);
+							break;
+						}
+						else
+						{
+							auto jt = it;
+							--jt;
+							mp.erase(it);
+							it = jt;
+						}
+					}
+				}
+				mp[p] = q;
 			}
 			ans += mp.size();
 		}
 		cout<<ans<<endl;
+
 	}
+
 	return 0;
 }
