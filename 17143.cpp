0e07f76417cfb701d7f3aa59a13bd63b035ca25b
DAMPER <44198800+dampers@users.noreply.github.com>
2022-01-14 23:31:48 +0900

    boj.kr/17143

---
+++ 17143.cpp
@@ -0,0 +1,148 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define swap(a,b) (a)^=(b)^=(a)^=(b)
+#define endl '\n'
+#define int long long
+
+using pii = pair<int, int>;
+
+const double EPS = 1e-9;
+const int dx[] = {0, 0, 1, -1};
+const int dy[] = {-1, 1, 0, 0};
+
+
+int r, c;
+
+class Shark
+{
+public:
+	int speed, dir, z;
+	Shark()
+	{
+		this->z = -1;
+		this->dir = -1;
+		this->speed = -1;
+	}
+	Shark(int speed, int dir, int z) : dir(dir), speed(speed), z(z) {}
+	void set(Shark K)
+	{
+		this->z = K.z;
+		this->dir = K.dir;
+		this->speed = K.speed;
+	}
+	void reset()
+	{
+		this->z = -1;
+		this->dir = -1;
+		this->speed = -1;
+	}
+};
+
+void printv(vector<vector<Shark>>& v)
+{
+	for(size_t i=0;i<v.size();i++)
+	{
+		for(size_t j=0;j<v[i].size();j++)
+		{
+			if(v[i][j].z == -1) cout << 0 << ' ';
+			else cout << v[i][j].z << ' ';
+		}
+		cout << endl;
+	}
+}
+
+void move(vector<vector<Shark>>& v, vector<vector<Shark>>& movev)
+{
+	for(size_t i=0;i<v.size();i++)
+	{
+		for(size_t j=0;j<v[i].size();j++)
+		{
+			if(v[i][j].z == -1) continue;
+			int ty = i, tx = j, tdir = v[i][j].dir, tz = v[i][j].z, tspeed = v[i][j].speed;
+			if(v[i][j].dir == 0 || v[i][j].dir == 1)
+			{
+				if(v[i][j].z == 4)
+				{
+					cout << ty << ' ' << tx << ' ' << tz << ' ' << tdir << endl;
+				}
+				if(v[i][j].dir == 0 && i != r-1) ty += r;
+				ty = (ty + v[i][j].speed) % (r+r);
+				tdir = 1;
+				if(ty >= r)
+				{
+					ty = (ty-(r+r-1)) * -1;
+					tdir = 0;
+				}
+
+			}
+			if(v[i][j].dir == 2 || v[i][j].dir == 3)
+			{
+				if(v[i][j].dir == 3 && j != c-1) tx += c;
+				tx = (tx+v[i][j].speed) % (c+c);
+				tdir = 2;
+				if(tx >= c)
+				{
+					tx = (tx-(c+c-1)) * -1;
+					tdir = 3;
+				}
+			}
+			if(movev[ty][tx].z < tz)
+			{
+				movev[ty][tx].set(v[i][j]);
+			}
+			v[i][j].reset();
+		}
+	}
+	printv(movev);
+	for(size_t i=0;i<movev.size();i++)
+	{
+		for(size_t j=0;j<movev[i].size();j++)
+		{
+			if(movev[i][j].z == -1) continue;
+			v[i][j].set(movev[i][j]);
+			movev[i][j].reset();
+		}
+	}
+}
+
+int32_t main()
+{
+	cin.sync_with_stdio(NULL);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int m;
+	cin >> r >> c >> m;
+	vector<vector<Shark>> v(r, vector<Shark>(c, Shark())), movev(r, vector<Shark>(c, Shark()));
+	int tr, tc, tspeed, tdir, tz;
+
+	for(int i=0;i<m;i++)
+	{
+		cin >> tr >> tc >> tspeed >> tdir >> tz;
+		v[tr-1][tc-1].speed = tspeed;
+		v[tr-1][tc-1].dir = tdir-1;
+		v[tr-1][tc-1].z = tz;
+	}
+	int ans = 0;
+
+	for(int fx=0;fx<c;fx++)
+	{
+		for(int fy=0;fy<r;fy++)
+		{
+			if(v[fy][fx].z != -1)
+			{
+				ans += v[fy][fx].z;
+				v[fy][fx].reset();
+				break;
+			}
+		}
+		printv(v);
+		cout << endl;
+		move(v, movev);
+		cout << ans << endl;
+	}
+	cout << ans;
+	return 0;
+}
