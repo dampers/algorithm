3eb3592ebc1014bd44999ded900346c70d6acbff
DAMPER <44198800+dampers@users.noreply.github.com>
2022-03-05 07:22:08 +0900

    Create 21964.cpp

---
+++ 21964.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define swap(a,b) (a)^=(b)^=(a)^=(b)
+#define endl '\n'
+#define int long long
+
+using pii = pair<int, int>;
+
+const double EPS = 1e-9;
+const int dx[] = {1, -1, 0, 0};
+const int dy[] = {0, 0, 1, -1};
+
+int32_t main()
+{
+	cin.sync_with_stdio(NULL);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+	string s;
+	cin >> s;
+	for(int i=s.length()-5;i<s.length();i++)
+		cout << s[i];
+
+	return 0;
+}
