c8ce8f2a4c88d6033cc2d909ad842212973b38b7
DAMPER <44198800+dampers@users.noreply.github.com>
2022-04-09 04:26:58 +0900

    Create 1965.cpp

---
+++ 1965.cpp
@@ -0,0 +1,40 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define swap(a, b) (a) ^= (b) ^= (a) ^= (b)
+#define endl '\n'
+#define int long long
+
+using pii = pair<int, int>;
+
+const double EPS = 1e-9;
+const int dx[] = {1, -1, 0, 0};
+const int dy[] = {0, 0, 1, -1};
+
+int32_t main()
+{
+    cin.sync_with_stdio(NULL);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+    vector<int> v(n, 0);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    vector<int> ans(1, 0);
+    for (int i = 0; i < n; i++)
+    {
+        if (ans.back() < v[i])
+            ans.emplace_back(v[i]);
+        else
+            *lower_bound(ans.begin(), ans.end(), v[i]) = v[i];
+    }
+    cout << ans.size() - 1;
+
+    return 0;
+}
