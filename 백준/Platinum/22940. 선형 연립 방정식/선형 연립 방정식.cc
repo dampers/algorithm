807162049dc2a47f635181636455defc254d39db
DAMPER <44198800+dampers@users.noreply.github.com>
2023-08-29 14:46:19 +0900

    [Platinum V] Title: 선형 연립 방정식, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Platinum/22940. 선형 연립 방정식/선형 연립 방정식.cc
+++ 백준/Platinum/22940. 선형 연립 방정식/선형 연립 방정식.cc
@@ -22,47 +22,32 @@ int32_t main()
     cin >> n;
     vector<vector<double>> matrix(n+1, vector<double>(n+1, 0.0));
     for(int i=0;i<n;i++)
-    {
         for(int j=0;j<=n;j++)
-        {
             cin >> matrix[i][j];
-        }
-    }
 
-    vector<double> ans(n, 0.0);
-    
     for(int i=0;i<n;i++)
     {
         for(int j=i+1;j<=n;j++)
-        {
             matrix[i][j] /= matrix[i][i];
-        }
         matrix[i][i] = 1.0;
-
         for(int j=i+1;j<n;j++)
         {
             double kp = matrix[j][i];
             vector<double> mkmatrix = matrix[i];
             for(int k=0;k<=n;k++)
-            {
                 matrix[j][k] -= (mkmatrix[k] * kp);
-            }
         }
     }
 
+    vector<double> ans(n, 0.0);
     ans[n-1] = matrix[n-1][n];
     for(int i=n-2;i>=0;i--)
     {
         ans[i] = matrix[i][n];
         for(int j=n-1;j>i;j--)
-        {
             ans[i] -= matrix[i][j] * ans[j];
-        }
     }
-
     for(int i=0;i<n;i++)
         cout << ans[i] << ' ';
-
-
     return 0;
 }
