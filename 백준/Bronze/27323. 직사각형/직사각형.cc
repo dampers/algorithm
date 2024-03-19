b4cd0fa75da0db07a1d09fa2ec2720ee2844c353
DAMPER <44198800+dampers@users.noreply.github.com>
2024-03-19 16:45:40 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n'
+#define int long long
+
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+const double EPS = 1e-9;
+const int dx[] = {1, -1, 0, 0};
+const int dy[] = {0, 0, 1, -1};
+
+int32_t main()
+{
+    cin.sync_with_stdio(NULL);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int a, b;
+    cin >> a >> b;
+    cout << a * b << endl;
+
+    return 0;
+}
