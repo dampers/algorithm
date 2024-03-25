8d4c2351f2139b2fff591adc594ef8af9ccfbc8c
DAMPER <44198800+dampers@users.noreply.github.com>
2024-03-25 17:03:14 +0900

    [Gold II] Title: 책 나눠주기, Time: 120 ms, Memory: 2172 KB -BaekjoonHub

--- 백준/Gold/9576. 책 나눠주기/책 나눠주기.cc
+++ 백준/Gold/9576. 책 나눠주기/책 나눠주기.cc
@@ -2,70 +2,75 @@
 
 using namespace std;
 
-#define swap(a,b) (a)^=(b)^=(a)^=(b)
 #define endl '\n'
 #define int long long
 
 using pii = pair<int, int>;
-
+using vi = vector<int>;
+using vvi = vector<vi>;
 const double EPS = 1e-9;
 const int dx[] = {1, -1, 0, 0};
 const int dy[] = {0, 0, 1, -1};
 
-bool dfs(int a, vector<int>& A, vector<int>& B, vector<pii>& range, vector<bool>& check)
+int N, M;
+int manOfBook[1001], bookOfMan[1001];
+bool ch[1001];
+
+bool dfs(int curr, vector<pii>& range)
 {
-	check[a] = true;
-	int left = range[a].first, right = range[a].second;
-	for(;left <= right;left++)
-	{
-		int b = left;
-		if(B[b] == 0 || (!check[B[b]] && dfs(B[b], A, B, range, check)))
-		{
-			A[a] = b;
-			B[b] = a;
-			return true;
-		}
-	}
-	return false;
+    ch[curr] = true;
+    int currBook = range[curr].first, right = range[curr].second;
+    for(;currBook <= right; currBook++)
+    {
+        if(manOfBook[currBook] == 0 || (!ch[manOfBook[currBook]] && dfs(manOfBook[currBook], range)))
+        {
+            bookOfMan[curr] = currBook;
+            manOfBook[currBook] = curr;
+            return true;
+        }
+    }
+    return false;
 }
 
 void solve()
 {
-	int n, m;
-	cin >> n >> m;
-	vector<int> A(m+1, 0), B(n+1, 0);
-	vector<pii> range(m+1);
-	for(int i=1;i<=m;i++)
-	{
-		cin >> range[i].first >> range[i].second;
-	}
+    cin >> N >> M;
+    
+    memset(manOfBook, 0, sizeof(manOfBook));
+    memset(bookOfMan, 0, sizeof(bookOfMan));
 
-	vector<bool> check(m+1, false);
-	int ans = 0;
+    vector<pii> range(M+3, {0, 0});
 
-	for(int i=1;i<=m;i++)
-	{
-		if(A[i] == 0)
-		{
-			fill(check.begin(), check.end(), false);
-			if(dfs(i, A, B, range, check)) ans++;
-		}
-	}
-	cout << ans << endl;
+    for(int i=1;i<=M;i++)
+    {
+        cin >> range[i].first >> range[i].second;
+    }
+
+    int ans = 0;
+    for(int i=1;i<=M;i++)
+    {
+        memset(ch, false, sizeof(ch));
+        if(dfs(i, range)) ans++;
+    }
+    cout << ans << endl;
+
 }
+
+
 
 int32_t main()
 {
-	cin.sync_with_stdio(NULL);
-	cin.tie(NULL);
-	cout.tie(NULL);
+    cin.sync_with_stdio(NULL);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-	int tc;
-	cin >> tc;
-	while(tc-- > 0)
-	{
-		solve();
-	}
+    int tc;
+    cin >> tc;
+    while(tc--)
+    {
+        solve();
+    }
+    
 
-	return 0;
+    return 0;
 }
